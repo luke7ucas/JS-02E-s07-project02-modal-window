fb541ef234a8c2bbfb11e6b5dddd02739ec94050
Lukasz <luke7ucas@gmail.com>
2021-12-21 01:09:48 +0000

    addEventListener

--- script.js
+++ script.js
@@ -6,6 +6,19 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-for(let i=0; i < btnsOpenModal.length; i++) 
-    console.log(btnsOpenModal[i].textContent);
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
